79932fd7dd0d765d79e951642157ca08987e6195
Daniel Alves <53535054+DanieAlves@users.noreply.github.com>
2023-05-12 16:56:45 -0300

    Ordenar Numeros

---
+++ SimuladoDesafio/OrdenarNumeros/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenarNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 2, 3, 5, 1, 63, 56, 34 };
+
+            for (int i = 0; i < numeros.Length - 1; i++)
+            {
+                for (int j = 0; j < numeros.Length - i - 1; j++)
+                {
+                    if (numeros[j] > numeros[j + 1])
+                    {
+                        int temp = numeros[j];
+                        numeros[j] = numeros[j + 1];
+                        numeros[j + 1] = temp;
+                    }
+                }
+            }
+
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
