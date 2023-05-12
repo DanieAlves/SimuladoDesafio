88ae79bb6ca891e9b823ac53afebce2c715642a5
Daniel Alves <53535054+DanieAlves@users.noreply.github.com>
2023-05-12 16:55:36 -0300

    palindromo

---
+++ SimuladoDesafio/Palindromo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite uma palavra:");
+            string palindromo = Console.ReadLine().Replace(" ", "").ToLower();
+            string palindromoReverse = new string(palindromo.ToCharArray().Reverse().ToArray());
+            
+            string saida = palindromo == palindromoReverse ? $"A string {palindromo} é um palíndromo" : $"A string {palindromo} não é um palíndromo";
+            Console.WriteLine(saida);
+
+            Console.ReadKey();
+        }
+    }
+}
